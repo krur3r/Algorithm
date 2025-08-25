1158b39543eea9e7b5f19723c400d4e87b8dd8e9
trtrngg <93835470+krur3r@users.noreply.github.com>
2025-08-25 14:13:26 +0700

    Update and rename B2_SNT_có_n_cso.py to exercises/n_chars_prime_number.py

--- exercises/n_chars_prime_number.py
+++ exercises/n_chars_prime_number.py
@@ -1,3 +1,4 @@
+#2. Viết chương trình tìm các số nguyên tố có N chữ số với N nhập từ bàn phím và 2 ≤ N ≤10.
 n = int(input("Nhập số chữ số n: "))
 if(n<2 or n>10):
     raise ValueError("Vui lòng nhập số n 2<=n<=10")

722ac4f6e02b23db2856aefdb9c7b61c6ea11b76
trtrngg <93835470+krur3r@users.noreply.github.com>
2025-08-25 15:15:09 +0700

    Update and rename B23.py to exercises/23.py
    in ra màn hình YES trong trường hợp tổng của các số nguyên tố trong khoảng [A, B] là cũng là một số nguyên tố và NO nếu ngược lại. Với A,B là hai số được nhập vào từ bàn phím.

--- exercises/23.py
+++ exercises/23.py
@@ -1,3 +1,4 @@
+#23. Viết chương trình in ra màn hình YES trong trường hợp tổng của các số nguyên tố trong khoảng [A, B] là cũng là một số nguyên tố và NO nếu ngược lại. Với A,B là hai số được nhập vào từ bàn phím. 
 from random import randint
 def dec_to_bin(k):
     r = []

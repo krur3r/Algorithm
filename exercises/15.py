80a7c22612ef2b66dd201d55aedeae31b02bc965
trtrngg <93835470+krur3r@users.noreply.github.com>
2025-08-25 14:38:42 +0700

    Update and rename B15_SNTsinhdoi.py to exercises/15.py

---
+++ exercises/15.py
@@ -0,0 +1,17 @@
+#15. Hai số nguyên tố sinh đôi là hai số nguyên tố hơn kém nhau 2 đơn vị. Viết chương trình tìm hai số nguyên tố sinh đôi nhỏ hơn hoặc bằng N, với N được nhập vào từ bàn phím.
+n=int(input("Nhập n: "))
+p=2
+prime=[1 for i in range(n+1)]
+while(p*p<=n):
+    for i in range(p*p,n+1,p):
+        prime[i]=0
+    p+=1
+seq_p=[]
+for p in range(2,n):
+    if(prime[p]==1):
+        seq_p.append(p)
+        p+=1
+print(seq_p)
+for i in range(1,len(seq_p)):
+    check=seq_p[i]-seq_p[i-1]
+    if(check==2):   print("Cặp SNT sinh đôi: ",seq_p[i-1],seq_p[i])     

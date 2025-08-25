4f24cc1c04c949c0279862d10191b4f5703b65be
trtrngg <93835470+krur3r@users.noreply.github.com>
2025-08-25 15:16:09 +0700

    Update and rename B24.py to exercises/24.py
    Đặt S1, S2 là các mảng chứa giá trị bình phương của các số nguyên tố. Hãy viết chương trình in ra số lượng tất cả các số nguyên tố nằm trong khoảng [a,b] sao cho số này cũng là tổng của hai số x và y với x thuộc S1 và y thuộc S2. Trong đó, a,b là các số được nhập từ bàn phím

--- exercises/24.py
+++ exercises/24.py
@@ -1,3 +1,7 @@
+# 24. Đặt S1, S2 là các mảng chứa giá trị bình phương của các số nguyên tố. Hãy viết chương trình in ra số lượng tất cả các số nguyên tố nằm trong khoảng [a,b] sao cho số này cũng là tổng của hai số x và y với x thuộc S1 và y thuộc S2. Trong đó, a,b là các số được nhập từ bàn phím
+# Ví dụ: với a=10, b =15, in ra giá trị là 1 vì trong khoảng [10,15] chỉ có 2 số nguyên tố 11 và 13,
+# nhưng chỉ có 13 = 2^2 + 3^2=4+9
+
 from random import randint
 from math import sqrt
 def dec_to_bin(k):
